47ecb21781f79001fb3c6bcbae85ac07b2735de7
Quentin ARNAULT <quentin.arnault@niji.fr>
2014-06-14 17:27:19 +0200

    OK, let's write some unit tests

---
+++ QACoreDataPlayground/QACoreDataPlaygroundTests/QACoreDataPlaygroundTests.m
@@ -0,0 +1,34 @@
+//
+//  QACoreDataPlaygroundTests.m
+//  QACoreDataPlaygroundTests
+//
+//  Created by Quentin Arnault on 14/06/2014.
+//  Copyright (c) 2014 Quentin Arnault. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface QACoreDataPlaygroundTests : XCTestCase
+
+@end
+
+@implementation QACoreDataPlaygroundTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
